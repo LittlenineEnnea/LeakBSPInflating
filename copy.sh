c6f6a9951c29fdad32a753cc1d260bafae4a2b96
Littlenine小九 <Aaron_root@outlook.com>
2025-05-12 18:09:23 +0800

    Add COPY.sh

---
+++ copy.sh
@@ -0,0 +1,10 @@
+cp -rp ./LA.QSSI*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./AUDIO*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./DISPLAY*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./VIDEO*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./XR*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./CV*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./SENSORS*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./GRAPHIC*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+cp -rp ./CAMERA*/LINUX/android/vendor/proprietary/* ./LA.VENDOR*/LINUX/android/vendor/proprietary/
+
